8cc45908f17ce7d84738cb2d8e14f3f46747c241
gditrani269 <gditrani269@gmail.com>
2020-11-04 00:17:55 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "germy - 16-09-2020 - Hello World!"
 
+@application.route("/mi")
+def mi():
+    return "soy el metodo mi"
+
 if __name__ == "__main__":
     application.run()
